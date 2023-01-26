815b4f595ddd7e62b99b0d638cc552a3a8ac054d
brenley-woodard <brenleyew@gmail.com>
2023-01-26 10:42:31 -0800

    in App.js I wrapped <App> in <GameProvider> and in GameContext.js I set initial board state and added export to utilize GameContext in components

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,11 +1,19 @@
-const { createContext } = require("react");
+import { createContext, useContext, useState } from 'react';
 
 const GameContext = createContext();
 const GameProvider = ({ children }) => {
-  
+  const [board, setBoard] = useState(boardData);
+
   return (
-    <GameContext.Provider value={{}}>
+    <GameContext.Provider value={{ board, setBoard }}>
       {children}
     </GameContext.Provider>
   );
-};
+}
+
+const useGameContext = () => {
+    const context= useContext(GameContext);
+    return context;
+}
+
+export { useGameContext, GameProvider }
