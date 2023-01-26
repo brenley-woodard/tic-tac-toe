ef7953538878822291a70ff1243ca02caed53fd6
brenley-woodard <brenleyew@gmail.com>
2023-01-26 13:01:49 -0800

    added handle click to box component

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Box.css';
 
+function handleClick() {
+  console.log('I am being clicked');
+}
+
 export default function Box() {
   return (
-    <div className='box'>Boxxx</div>
+    <div className='box' onClick={handleClick}>Boxxx</div>
   );
 }
