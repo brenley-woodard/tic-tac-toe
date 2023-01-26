20d70c702ab36a37be2716d83dda27aabf60b1de
brenley-woodard <brenleyew@gmail.com>
2023-01-26 12:48:44 -0800

    added flex box styling to App, added grid styling to board, and added flex styling to box

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Board.css';
 import Box from '../Box/Box';
 import boardData from '../../board-data';
 
 export default function Board() {
   return (
-    <div>
+    <div className='board'>
       {boardData.map((box) => (
         <Box
           key={box.space + box.content}
