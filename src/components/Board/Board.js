422b4af5fdd11aecb4ec9b059fd266836bc02c98
brenley-woodard <brenleyew@gmail.com>
2023-01-26 12:19:05 -0800

    added state to GameContext, created Header component, created box component, created Board componenet, created board data with 9 spaces, and called Header and Board to App.js

---
+++ src/components/Board/Board.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Box from '../Box/Box';
+
+export default function Board() {
+  return (
+    <div>
+      <Box></Box>
+    </div>
+  );
+}
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,19 +1,24 @@
 import { createContext, useContext, useState } from 'react';
+import boardData from '../board-data';
 
 const GameContext = createContext();
+
 const GameProvider = ({ children }) => {
   const [board, setBoard] = useState(boardData);
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [active, setActive] = useState(true);
+  const [gameMessage, setGameMessage] = useState('');
 
   return (
-    <GameContext.Provider value={{ board, setBoard }}>
+    <GameContext.Provider value={{ board, setBoard, currentPlayer, setCurrentPlayer, active, setActive, gameMessage, setGameMessage }}>
       {children}
     </GameContext.Provider>
   );
-}
+};
 
 const useGameContext = () => {
-    const context= useContext(GameContext);
-    return context;
-}
+  const context = useContext(GameContext);
+  return context;
+};
 
-export { useGameContext, GameProvider }
+export { useGameContext, GameProvider };
