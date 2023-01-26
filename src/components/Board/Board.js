10e6d36e58ecc293108c5d364e5ef8d8566e6854
brenley-woodard <brenleyew@gmail.com>
2023-01-26 12:32:31 -0800

    in Board.js I am mapping through boardData and am rendering all 9 boxes to page

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Box from '../Box/Box';
+import boardData from '../../board-data';
 
 export default function Board() {
   return (
     <div>
-      <Box></Box>
+      {boardData.map((box) => (
+        <Box
+          key={box.space + box.content}
+        />
+      ))}
     </div>
   );
 }
