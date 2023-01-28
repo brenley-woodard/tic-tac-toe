1d28e3c0ae3bc5379157718b0aa1303aaa3706b7
brenley-woodard <brenleyew@gmail.com>
2023-01-27 17:03:55 -0800

    I moved board data into GameContext and created game logic that stops game when a player has 3 in a row

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,10 +1,18 @@
 import { createContext, useContext, useState } from 'react';
-import boardData from '../board-data';
-
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
-  const [board, setBoard] = useState(boardData);
+  const [board, setBoard] = useState([
+    { space: 0, content: '' },
+    { space: 1, content: '' },
+    { space: 2, content: '' },
+    { space: 3, content: '' },
+    { space: 4, content: '' },
+    { space: 5, content: '' },
+    { space: 6, content: '' },
+    { space: 7, content: '' },
+    { space: 8, content: '' },
+  ]);
   const [currentPlayer, setCurrentPlayer] = useState('X');
   const [active, setActive] = useState(true);
   const [gameMessage, setGameMessage] = useState('');
@@ -17,22 +25,44 @@ const GameProvider = ({ children }) => {
       return box;
     });
     setBoard(newBoard);
-    checkWin(newBoard);
+    checkWin();
   };
  
   const checkWin = () => {
-    if ((board[0].content === board[1].content && board[2].content))console.log('WINNER');
-    if ((board[3].content === board[4].content && board[5].content))console.log('WINNER');
-    if ((board[6].content === board[7].content && board[8].content))console.log('WINNER');
-    if ((board[0].content === board[3].content && board[6].content))console.log('WINNER');
-    if ((board[1].content === board[4].content && board[7].content))console.log('WINNER');
-    if ((board[2].content === board[5].content && board[8].content))console.log('WINNER');
-    if ((board[0].content === board[4].content && board[8].content))console.log('WINNER');
-    if ((board[2].content === board[4].content && board[6].content))console.log('WINNER');
+    if ((board[0].content === board[1].content) & (board[0].content !== '') & (board[0].content === board[2].content))
+      setActive(false);
+    // console.log('Winner!');
+    if ((board[3].content === board[4].content) & (board[3].content !== '') & (board[3].content === board[5].content))
+      setActive(false);
+    // console.log('Winner!');
+    
+    if ((board[6].content === board[7].content) & (board[6].content !== '') & (board[6].content === board[8].content))
+      setActive(false);
+    // console.log('Winner!');
+    
+    if ((board[0].content === board[3].content) & (board[0].content !== '') & (board[0].content === board[6].content))
+      setActive(false);
+    // console.log('Winner!');
+    
+    if ((board[1].content === board[4].content) & (board[1].content !== '') & (board[1].content === board[7].content))
+      setActive(false);
+    // console.log('Winner!');
+    
+    if ((board[2].content === board[5].content) & (board[2].content !== '') & (board[2].content === board[8].content))
+      setActive(false);
+    // console.log('Winner!');
+    
+    if ((board[0].content === board[4].content) & (board[0].content !== '') & (board[0].content === board[8].content))
+      setActive(false);
+    // console.log('Winner!');
+  
+    if ((board[2].content === board[4].content) & (board[2].content !== '') & (board[2].content === board[6].content))
+      setActive(false);
+    // console.log('Winner!');
   };
 
   return (
-    <GameContext.Provider value={{ board, setBoard, currentPlayer, setCurrentPlayer, active, setActive, gameMessage, setGameMessage, handleClick }}>
+    <GameContext.Provider value={{ board, setBoard, currentPlayer, setCurrentPlayer, active, setActive, gameMessage, setGameMessage, handleClick, checkWin }}>
       {children}
     </GameContext.Provider>
   );
